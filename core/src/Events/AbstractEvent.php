148726d5efd036b5fa114368c1d8272513991b37
vgrish <vgrish@yandex.ru>
2025-07-07 14:54:16 +0300

    Remove OnSiteRefreshEvent event

--- core/src/Events/AbstractEvent.php
+++ core/src/Events/AbstractEvent.php
@@ -4,22 +4,9 @@ declare(strict_types=1);
 
 namespace MXRVX\ORM\Events;
 
-use Cycle\ORM\ORMInterface;
-use MXRVX\ORM\App;
-
 abstract class AbstractEvent
 {
     public function __construct(protected \modX &$modx, protected array $properties = []) {}
 
     abstract public function __invoke(): void;
-
-    protected function reloadCache(): void
-    {
-        App::clearCache();
-
-        //NOTE: Reload ORM for new cache schema
-        /** @var \DI\Container $container */
-        $container = \MXRVX\Autoloader\App::getInstance($this->modx)->getContainer();
-        $container->get(ORMInterface::class);
-    }
 }
--- core/src/Events/OnCacheUpdateEvent.php
+++ core/src/Events/OnCacheUpdateEvent.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace MXRVX\ORM\Events;
 
+use Cycle\ORM\ORMInterface;
+use MXRVX\ORM\App;
+
 class OnCacheUpdateEvent extends AbstractEvent
 {
     public function __invoke(): void
     {
-        $this->reloadCache();
+        App::clearCache();
+
+        //NOTE: Reload ORM for new cache schema
+        /** @var \DI\Container $container */
+        $container = \MXRVX\Autoloader\App::getInstance($this->modx)->getContainer();
+        $container->get(ORMInterface::class);
     }
 }
